f0945906b9f3c6fde2d2184011f697ece7b56dd2
Maarten Knapen <maarten.knapen@telenet.be>
2019-05-22 09:00:36 +0200

    added AddTicket tests

--- src/main/java/be/jidoka/clean/code/labs/movie/MovieBill.java
+++ src/main/java/be/jidoka/clean/code/labs/movie/MovieBill.java
@@ -5,19 +5,36 @@ import java.time.DayOfWeek;
 public class MovieBill implements Bill {
 
     double billTotal;
+    int numberOfTickets;
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
 
     @Override
     public void startPurchase(int runtime, DayOfWeek dayOfWeek, boolean loge, boolean threeD) {
-
+        billTotal = 0;
     }
 
     @Override
     public void addTicket(int age, boolean student) {
-
+        if (student) {
+            billTotal += 8.00;
+        } else {
+            if (age >= 65) {
+                billTotal += 6.00;
+            } else if (age < 13) {
+                billTotal += 5.50;
+            } else {
+                billTotal += 11.00;
+            }
+        }
+        numberOfTickets += 1;
     }
+
 
     @Override
     public double finishPurchase() {
-        return 0;
+        return billTotal;
     }
 }
--- src/test/java/be/jidoka/clean/code/labs/movie/MovieTests.java
+++ src/test/java/be/jidoka/clean/code/labs/movie/MovieTests.java
@@ -1,5 +1,7 @@
 package be.jidoka.clean.code.labs.movie;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.DayOfWeek;
@@ -8,13 +10,72 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MovieTests {
 
+    Bill bill;
+
+    @BeforeEach
+    void init() {
+        bill = new MovieBill();
+    }
+
 
     @Test
-    void TotalBillIsZeroWhenStartingAPurchase() {
-        Bill bill = new MovieBill();
+    @DisplayName("When starting a purchase the total bill should be zero")
+    void StartAPurchase() {
         bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
         assertThat(bill.finishPurchase()).isEqualTo(0.0);
     }
 
+    @Test
+    @DisplayName("When adding a general admission Ticket, the total bill value should be '11,00'.")
+    void AddGeneralAdmissionTicket() {
+        bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
+        bill.addTicket(18, false);
+        assertThat(bill.finishPurchase()).isEqualTo(11.00);
+    }
 
+    @Test
+    @DisplayName("When adding 2 general admission tickets, the total bill value should be '22.00'.")
+    void AddMultipleGeneralAdmissionTickets() {
+        bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
+        bill.addTicket(18, false);
+        bill.addTicket(18, false);
+
+        assertThat(bill.finishPurchase()).isEqualTo(22.00);
+    }
+
+    @Test
+    @DisplayName("When adding a student ticket, the total bill value should be '8.00'.")
+    void AddStudentTicket(){
+        bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
+        bill.addTicket(19, true);
+
+        assertThat(bill.finishPurchase()).isEqualTo(8.00);
+    }
+
+    @Test
+    @DisplayName("When adding a senior citizen project, the total bill value should be '6.00'.")
+    void AddSeniorCitizenTicket(){
+        bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
+        bill.addTicket(66, false);
+
+        assertThat(bill.finishPurchase()).isEqualTo(6.00);
+    }
+
+    @Test
+    @DisplayName("When adding a children's ticket, the total bill value should be '5.50'. ")
+    void AddChildrensTicket(){
+        bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
+        bill.addTicket(11, false);
+
+        assertThat(bill.finishPurchase()).isEqualTo(5.50);
+    }
+
+    @Test
+    @DisplayName("When adding a ticket, the number of tickets is increased by 1")
+    void AddATicket(){
+        bill.startPurchase(90, DayOfWeek.MONDAY, false, false);
+        bill.addTicket(11, false);
+
+       // assertThat()
+    }
 }
